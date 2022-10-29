b475ad696ac1cd9bb17aef472cc0e8a732cc9638
akkinenigopichand <98510781+akkinenigopichand@users.noreply.github.com>
2022-10-29 20:24:04 +0530

    Add files via upload

---
+++ EmployeeServiceRestGraded Project/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into roles (role_id,name) values (1,'ADMIN');
+
+insert into users (user_id,password,username) values (1,'Random$Complex@Password','ujjawal');
+
+insert into users_roles (user_id,role_id) values (1,1);
